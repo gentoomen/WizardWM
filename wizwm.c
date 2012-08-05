0e35da0a1a164dca0e6b5f9b6329e5f51f1fe3c4
Longlius <randomticktock@gmail.com>
2012-08-05 03:45:34 -0500

    Added a main header file and began writing wizwm.c

--- wizwm.c
+++ wizwm.c
@@ -1,7 +1,13 @@
-/* Test comment by Longlius. Still figuring out github so feel free to delete. */
+#include "wizwm.h"
 
 int main(int argc, char **argv)
 {
-
+	/* Open a connection to the X server */
+	xcb_connection_t *connection = xcb_connect(NULL, NULL);
+	if (!connection) {
+		fprintf(stderr, "%s", "ERROR: Can't connect to the X server!\n");
+		return 1;
+	}
+	
 	return 0;
 }
