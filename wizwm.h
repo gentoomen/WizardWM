b1305c014dbcd5ef35c42cdcb758c5ab9ab80f8c
Longlius <randomticktock@gmail.com>
2012-08-05 03:44:40 -0500

    Added a main header file and began writing wizwm.c

---
+++ wizwm.h
@@ -0,0 +1,12 @@
+/* Main header file for WizardWM */
+#ifndef WIZWM_H_	// prevent multiple inclusion with PREPROCESSER MAGIKS
+#define WIZWM_H_
+
+/* XCB header file */
+#include <xcb/xcb.h>
+
+/* ORIGINAL MACROS DO NOT STEAL! */
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+#endif
