e958574277ff89e1710a998d38ee7b545c9dc892
Longlius <randomticktock@gmail.com>
2012-08-05 04:13:07 -0500

    Figured out how to get the root of the first screen. XCB IS HARD

--- wizwm.c
+++ wizwm.c
@@ -1,13 +1,19 @@
 #include "wizwm.h"
 
 int main(int argc, char **argv)
-{
-	/* Open a connection to the X server */
-	xcb_connection_t *connection = xcb_connect(NULL, NULL);
-	if (!connection) {
-		fprintf(stderr, "%s", "ERROR: Can't connect to the X server!\n");
+{	
+	xcb_connection_t *disp;
+	xcb_screen_t *scr;
+	xcb_drawable_t rt;
+	
+	/* Attempt to connect to X server, exit with error if unsuccessful */
+	disp = xcb_connect(NULL, NULL);
+	if (xcb_connection_has_error(disp))
 		return 1;
-	}
+	
+	/* Get root of first screen. Currently only supports one screen. */
+	scr = xcb_setup_roots_iterator(xcb_get_setup(disp)).data;
+	rt = scr->root;
 	
 	return 0;
 }
