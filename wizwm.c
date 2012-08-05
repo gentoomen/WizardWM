221292c2e606c3d0d8dd71fa48892c3f864c0fe0
David Thompson <dthompson@member.fsf.org>
2012-08-05 13:09:47 -0400

    Add basic window manager detection.

--- wizwm.c
+++ wizwm.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "wizwm.h"
 
 int main(int argc, char **argv)
@@ -14,6 +16,41 @@ int main(int argc, char **argv)
 	/* Get root of first screen. Currently only supports one screen. */
 	scr = xcb_setup_roots_iterator(xcb_get_setup(disp)).data;
 	rt = scr->root;
+
+	/* There can be only one! */
+	if(detect_window_manager(disp, scr))
+	{
+	     printf("Another window manager is currntly running\n");
+	     return 1;
+	}
 	
 	return 0;
+}
+
+static xcb_window_t detect_window_manager(xcb_connection_t *connection, xcb_screen_t *screen)
+{
+     /* Section 4.3 of the ICCCM standard states that a window manager should */
+     /* own the selection named WM_Sn, where n is the screen number.          */
+     /* char atom_name[6]; */
+     /* sprintf(atom_name, "WM_S%d", screen->root); */
+     
+     /* Currently only uses screen 0. */
+     const char *atom_name = "WM_S0";
+
+     /* This should probably be made it's own function for convenience. */
+     xcb_intern_atom_cookie_t atom_cookie = xcb_intern_atom(connection, 0, strlen(atom_name), atom_name);
+     xcb_intern_atom_reply_t *atom_reply = xcb_intern_atom_reply(connection, atom_cookie, NULL);
+     xcb_atom_t selection = atom_reply->atom;
+
+     /* Get selection owner. */
+     xcb_get_selection_owner_cookie_t selection_owner_cookie = xcb_get_selection_owner(connection, selection);
+     xcb_get_selection_owner_reply_t *selection_owner_reply = xcb_get_selection_owner_reply(connection, selection_owner_cookie, NULL);
+     xcb_window_t selection_owner = 0;
+     if(selection_owner_reply)
+	  selection_owner = selection_owner_reply->owner;
+
+     free(atom_reply);
+     free(selection_owner_reply);
+
+     return selection_owner;
 }
--- wizwm.h
+++ wizwm.h
@@ -2,11 +2,18 @@
 #ifndef WIZWM_H_	// prevent multiple inclusion with PREPROCESSER MAGIKS
 #define WIZWM_H_
 
-/* XCB header file */
+/* Standard library */
+#include <stdlib.h>
+#include <string.h>
+
+/* XCB header files */
 #include <xcb/xcb.h>
 
 /* ORIGINAL MACROS DO NOT STEAL! */
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+/* Detect presence of another window manager */
+static xcb_window_t detect_window_manager(xcb_connection_t *connection, xcb_screen_t *screen);
 
 #endif
