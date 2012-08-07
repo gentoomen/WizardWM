6a18e320463d0a46265ca3dd96d96a7ec7f043d2
Marko Ceferin <mceferin@gmail.com>
2012-08-07 17:21:05 +0200

    fixed formatting
    We use 1TBS with tabs for indentation. To do it with astyle:
    astyle --style=1tbs --indent=tab --indent-switches
    Vim function (:Astyle) that formats the current buffer:
    http://sprunge.us/fXAB

--- wizwm.c
+++ wizwm.c
@@ -1,28 +1,26 @@
 #include "screen.h"
 
 int main(int argc, char **argv)
-{	
-     xcb_connection_t *connection = connectToXServer();
-	
-     /* Iterate over each screen and attempt to manage it */
-     /* This is quite verbose but I can't find a more elegant way to do it */
-     const xcb_setup_t *setup = xcb_get_setup(connection);
-     xcb_screen_iterator_t iter = xcb_setup_roots_iterator(setup);
-     for(int i = 0; i < xcb_setup_roots_length(setup); ++i)
-     {
-	  xcb_screen_t *screen = iter.data;
+{
+	xcb_connection_t *connection = connectToXServer();
 
-	  /* There can be only one! */
-	  if(detect_window_manager(connection, screen, i))
-	  {
-	       fprintf(stderr, "Another window manager is currntly running on screen %d\n", i);
-	       return 1;
-	  }
+	/* Iterate over each screen and attempt to manage it */
+	/* This is quite verbose but I can't find a more elegant way to do it */
+	const xcb_setup_t *setup = xcb_get_setup(connection);
+	xcb_screen_iterator_t iter = xcb_setup_roots_iterator(setup);
+	for(int i = 0; i < xcb_setup_roots_length(setup); ++i) {
+		xcb_screen_t *screen = iter.data;
 
-	  printf("Screen %d are belong to us\n", i);
-	  init(connection, screen);
-	  xcb_screen_next(&iter);
-     }
+		/* There can be only one! */
+		if(detect_window_manager(connection, screen, i)) {
+			fprintf(stderr, "Another window manager is currntly running on screen %d\n", i);
+			return 1;
+		}
 
-     return 0;
+		printf("Screen %d are belong to us\n", i);
+		init(connection, screen);
+		xcb_screen_next(&iter);
+	}
+
+	return 0;
 }
